e48699842a8029e33ab44e4faf1e557c59309f95
SpikeCifer <stelioszapp+dev@gmail.com>
2023-03-19 16:59:19 +0200

    Added dual mode in script

--- kiwi-source/execute.sh
+++ kiwi-source/execute.sh
@@ -1,7 +1,16 @@
-requests=10000
+#!/bin/bash
+requests=10000000
+mode=0
 make all
 cd bench
-./kiwi-bench mix $requests 
+
+if [ "$mode" -eq 1 ]
+then
+    ./kiwi-bench write $requests 
+    ./kiwi-bench read $requests 
+else
+    gdb --args kiwi-bench mix $requests 
+fi
 
 cd ..
 make clean
